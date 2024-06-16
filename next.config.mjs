35bf078769f155b00e6dd3191ac9c2df96a046ac
FaizQowy <67276767+miurayuuki05@users.noreply.github.com>
2024-06-16 18:24:15 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,7 @@ const nextConfig = {
     env : {
         MONGODB_LINK : "mongodb+srv://FaizQowy:Faizqowy1.@pbo-cluster.jxh8pub.mongodb.net/?retryWrites=true&w=majority&appName=pbo-cluster",
         TOKEN_SECRET : "ThisIsaSecretToken_22828882888ijefubduvsiudbvyerb",
-        UID_SECRET : "wdijefuuhuh3424183573hf74fhu3fh73hf4g9h9h9h00j10r0jf12",
-        MERCHANT_ID : "G357843545",
-        SERVER_KEY : "SB-Mid-server-UGfZfu0hCmribNWHEb5oqeyv"
+        UID_SECRET : "wdijefuuhuh3424183573hf74fhu3fh73hf4g9h9h9h00j10r0jf12"
     }
 };
 
